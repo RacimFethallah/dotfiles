7362c3a9f6fdc790900a4ea76382f77609692a90
Ric's Dev <ricsdev@proton.me>
2026-07-13 19:56:48 +0100

    added ether theme

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -1,11 +1,11 @@
 vim.pack.add({
-	'https://github.com/nvim-mini/mini.pairs',
+	-- 'https://github.com/nvim-mini/mini.pairs',
 	'https://github.com/nvim-mini/mini.ai',
 	'https://github.com/nvim-mini/mini.surround',
 	'https://github.com/nvim-mini/mini.clue'
 })
 
-require('mini.pairs').setup()
+-- require('mini.pairs').setup()
 require('mini.ai').setup()
 require('mini.surround').setup()
 
--- theme-switcher.sh
+++ theme-switcher.sh
@@ -4,8 +4,10 @@ theme="$1"
 
 unlink ~/dotfiles/ghostty/themes/current        ; ln -sf "$HOME/dotfiles/ghostty/themes/$theme"         ~/dotfiles/ghostty/themes/current
 unlink ~/dotfiles/alacritty/themes/current.toml ; ln -sf "$HOME/dotfiles/alacritty/themes/$theme.toml"  ~/dotfiles/alacritty/themes/current.toml
-unlink ~/dotfiles/helix/themes/current.toml     ; ln -sf "$HOME/dotfiles/helix/themes/$theme.toml"      ~/dotfiles/helix/themes/current.toml
-unlink ~/dotfiles/nvim/lua/themes/current       ; ln -sf "$HOME/dotfiles/nvim/lua/themes/$theme"        ~/dotfiles/nvim/lua/themes/current
+# unlink ~/dotfiles/helix/themes/current.toml     ; ln -sf "$HOME/dotfiles/helix/themes/$theme.toml"      ~/dotfiles/helix/themes/current.toml
+# unlink ~/dotfiles/nvim/lua/themes/current       ; ln -sf "$HOME/dotfiles/nvim/lua/themes/$theme"        ~/dotfiles/nvim/lua/themes/current
+unlink ~/dotfiles/helix/themes/current.toml     ; ln -sf "$HOME/dotfiles/helix/themes/flow.toml"      ~/dotfiles/helix/themes/current.toml
+unlink ~/dotfiles/nvim/lua/themes/current       ; ln -sf "$HOME/dotfiles/nvim/lua/themes/flow"        ~/dotfiles/nvim/lua/themes/current
 
 echo "Theme switched to $theme"
 
